6ae47c94c68ab0f470de1a529fd86012b175d243
ANJONGGIL <qws458@naver.com>
2023-04-14 15:12:51 +0900

    10장
    - spring 적용중

---
+++ framwork/src/test/java/dev/davivieira/topologyinventory/framwork/input/generic/NetworkManagementGenericAdapterTest.java
@@ -0,0 +1,21 @@
+package dev.davivieira.topologyinventory.framwork.input.generic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NetworkManagementGenericAdapterTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addNetworkToSwitch() {
+    }
+
+    @Test
+    void removeNetworkFromSwitch() {
+    }
+}
---
+++ framwork/src/test/java/dev/davivieira/topologyinventory/framwork/output/data/SwitchDataTest.java
@@ -0,0 +1,44 @@
+package dev.davivieira.topologyinventory.framwork.output.data;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SwitchDataTest {
+
+    @Test
+    void getSwitchId() {
+    }
+
+    @Test
+    void getRouterId() {
+    }
+
+    @Test
+    void getSwitchVendor() {
+    }
+
+    @Test
+    void getSwitchModel() {
+    }
+
+    @Test
+    void getSwitchType() {
+    }
+
+    @Test
+    void getNetworks() {
+    }
+
+    @Test
+    void getIp() {
+    }
+
+    @Test
+    void getSwitchLocation() {
+    }
+
+    @Test
+    void builder() {
+    }
+}
