0fd8ad5c6c3fcf3767ecdaf349854528227d12cf
ANJONGGIL <qws458@naver.com>
2023-04-15 13:29:52 +0900

    10장
    - spring 적용중

---
+++ framwork/src/test/java/dev/davivieira/topologyinventory/framwork/input/generic/RouterManagementGenericAdapterTest.java
@@ -0,0 +1,33 @@
+package dev.davivieira.topologyinventory.framwork.input.generic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RouterManagementGenericAdapterTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void retrieveRouter() {
+    }
+
+    @Test
+    void removeRouter() {
+    }
+
+    @Test
+    void createRouter() {
+    }
+
+    @Test
+    void addRouterToCoreRouter() {
+    }
+
+    @Test
+    void removeRouterFromCoreRouter() {
+    }
+}
---
+++ framwork/src/test/java/dev/davivieira/topologyinventory/framwork/input/generic/SwitchManageGenericAdapterTest.java
@@ -0,0 +1,21 @@
+package dev.davivieira.topologyinventory.framwork.input.generic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SwitchManageGenericAdapterTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void retrieveSwitch() {
+    }
+
+    @Test
+    void createAndAddSwitchToEdgeRouter() {
+    }
+}
